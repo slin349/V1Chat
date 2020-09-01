d7ff9dc087adf41fbb288cecc832dbae60a19e98
Steven <stevenlin159@gmail.com>
2020-09-01 16:50:17 -0400

    Working chat room

--- client.js
+++ client.js
@@ -1,15 +1,33 @@
-const socket = io('http://localhost:3000'); //this is where the server is hosted on
 //script.js is referring to the actual webpage
+const socket = io('http://localhost:3000'); //this is where the server is hosted on
+const messageContainer = document.getElementById('message-container');
 const messageForm = document.getElementById('send-container');
 const messageInput = document.getElementById('message-input');
 
+const name = prompt('What is your name?');
+appendMessage(`Welcome to the chat room, ${name}!`); //welcome message
+socket.emit('new-user', name); //sends data back to server
+
 socket.on('chat-message', data => { //whenever we receive a socket event named 'chat-message', print out the data
-    console.log(data);
+    appendMessage(data);
+}); 
+
+socket.on('user-connected', name => { //whenever we receive a socket event named 'user-connected', welcome them
+    appendMessage(`${name} has joined the room`);
 }); 
 
 messageForm.addEventListener('submit', e => { //whenever we submit form, we stop form from submitting
     e.preventDefault(); //this prevents page from refreshing
-    const message = messageInput.value;
+
+    const message = messageInput.value; //create variable to store the text value
+
     socket.emit('send-chat-message', message); //this creates a socket with the send-chat-message as keyword and sends the message variable as data
+
     messageInput.value = ''; //empties the value after its been sent
 }); 
+
+function appendMessage(message){ //used to create text box when people send messages
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageContainer.append(messageElement); //add the element to the container, in this case another div
+}
--- server.js
+++ server.js
@@ -1,11 +1,18 @@
-const io = require('socket.io')(3000);
 //server.js is basically referring to this console and not the webpage
+const io = require('socket.io')(3000);
 
-io.on('connection', socket => { //when user joins website, it sends message 'Hello World'
-    console.log('User connected')
-    socket.emit('chat-message', 'Hello World') //'chat-message' is the event name, 'Hello World' is the data being sent
+const users = {}
+
+io.on('connection', socket => { //when user joins website
+
+    socket.on('new-user', name =>{
+        console.log(`${name} connected to chat room`);
+        users[socket.id] = name;
+        socket.broadcast.emit('user-connected', name);
+    })
 
     socket.on('send-chat-message', message => { //waits for this socket to be called and sends out the message coming from the client side
         console.log(message);
+        socket.broadcast.emit('chat-message', message); //sends message to everyone thats connected on website except for the person that sent the message
     })
 }); 
