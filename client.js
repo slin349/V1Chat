12bbeff433debd05b62c195ece10499de4405be0
Steven <stevenlin159@gmail.com>
2020-09-01 17:03:00 -0400

    Working chat room without finished css

--- client.js
+++ client.js
@@ -9,20 +9,22 @@ appendMessage(`Welcome to the chat room, ${name}!`); //welcome message
 socket.emit('new-user', name); //sends data back to server
 
 socket.on('chat-message', data => { //whenever we receive a socket event named 'chat-message', print out the data
-    appendMessage(data);
+    appendMessage(`${data.name}: ${data.message}`); //Calls on the function and included the name as well as the text being sent
 }); 
 
 socket.on('user-connected', name => { //whenever we receive a socket event named 'user-connected', welcome them
     appendMessage(`${name} has joined the room`);
 }); 
 
+socket.on('user-disconnected', name => { //when user disconnects show message
+    appendMessage(`${name} has left the room`);
+}); 
+
 messageForm.addEventListener('submit', e => { //whenever we submit form, we stop form from submitting
     e.preventDefault(); //this prevents page from refreshing
-
     const message = messageInput.value; //create variable to store the text value
-
+    appendMessage(`You: ${message}`);
     socket.emit('send-chat-message', message); //this creates a socket with the send-chat-message as keyword and sends the message variable as data
-
     messageInput.value = ''; //empties the value after its been sent
 }); 
 
--- server.js
+++ server.js
@@ -6,13 +6,17 @@ const users = {}
 io.on('connection', socket => { //when user joins website
 
     socket.on('new-user', name =>{
-        console.log(`${name} connected to chat room`);
+        console.log(`${name} connected`);
         users[socket.id] = name;
         socket.broadcast.emit('user-connected', name);
     })
 
     socket.on('send-chat-message', message => { //waits for this socket to be called and sends out the message coming from the client side
-        console.log(message);
-        socket.broadcast.emit('chat-message', message); //sends message to everyone thats connected on website except for the person that sent the message
+        socket.broadcast.emit('chat-message', {message: message, name: users[socket.id]}); //sends message to everyone thats connected on website except for the person that sent the message
+    })
+
+    socket.on('disconnect', () => { //when user disconnects send that name data to client side and delete user from array
+        socket.broadcast.emit('user-disconnected', users[socket.id]);
+        delete users[socket.id];
     })
 }); 
